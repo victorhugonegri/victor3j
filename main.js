979b1f203146de41a1ee3c06994ba8a09dbd0ef2
victorhugonegri <110132707+victorhugonegri@users.noreply.github.com>
2024-06-06 08:06:02 -0300

    06-06

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const botoes  = document.querySelectorAll(".botao");
-console.log (botoes)
+const conteudo  = document.querySelectorAll(".aba-conteudo");
 for (let i = 0; i < botoes.length ; i++){
-    botoes[i].onclick = function() {
-        for (j=0;j<botoes.length;j++){
-            botoes[j].classList.remove("ativo");
-        }
-        botoes[i].classList.add("ativo")
-    } 
+    botoes[i].onclick = function(){
+    for (j=0;j<botoes.length;j++){
+        botoes[j].classList.remove("ativo");
+        conteudo[j].classList.remove("ativo")
+    }
+    botoes[i].classList.add("ativo")
+    conteudo[i].classList.add("ativo")
+  }
 }
