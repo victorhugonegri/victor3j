b922c2be45608dc638c9e4c2db261be829b2824f
victorhugonegri <110132707+victorhugonegri@users.noreply.github.com>
2024-06-14 09:44:00 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
-const botoes  = document.querySelectorAll(".botao");
-const conteudo  = document.querySelectorAll(".aba-conteudo");
-for (let i = 0; i < botoes.length ; i++){
+const botoes = document.querySelectorAll(".botao");
+const conteudo = document.querySelectorAll(".aba-conteudo")
+const tempo = document.querySelectorAll(".contador");
+tempo[0].textContent="Olá";
+const tempoObjetivo1=new Date (2007,11,22,05,40,30);
+tempo[1].textContent=tempoObjetivo1;
+const tempoObjetivo2=new Date(28,06);
+const agora=new Date();
+console.log(tempo);
+for(let i = 0; i < botoes.length ; i++){
     botoes[i].onclick = function(){
-    for (j=0;j<botoes.length;j++){
-        botoes[j].classList.remove("ativo");
-        conteudo[j].classList.remove("ativo")
+        for (let j = 0 ; j < botoes.length ; j++){
+            botoes[j].classList.remove("ativo");
+            conteudo[j].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo")
+        conteudo[i].classList.add("ativo")
     }
-    botoes[i].classList.add("ativo")
-    conteudo[i].classList.add("ativo")
-  }
 }
