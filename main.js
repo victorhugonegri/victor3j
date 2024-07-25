11f4c1326b6834b58e4200656446def9022c976d
victorhugonegri <110132707+victorhugonegri@users.noreply.github.com>
2024-07-25 08:08:30 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -6,6 +6,23 @@ const tempoObjetivo1 = new Date(2007,06);
 const tempoObjetivo2 = new Date(2024,06);
 const agora = new Date();
 let segundos ;
+let minutos; 
+let horas;
+let dias;
+segundos=Math.floor((tempoObjetivo-agora)/1000);
+minutos=Math.floor(segundos/60);
+horas=Math.floor(minutos/60);
+dias=Math.floor(horas/24);
+
+segundos = segundos%60
+minutos = minutos%60
+horas = horas%24
+
+if(segundos >=0){ 
+return`faltam${dias}dias,${horas}horas,${minutos}minutos e${segundos}´
+}else{
+  return'Prazo Finalizado!'
+}
 segundos = (tempoObjetivo1 - agora)/1000;
 let minutos;
 minutos = segundos/60;
